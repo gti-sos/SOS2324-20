d48fcc44c76f4792fe594989f2fa98eb552971b3
C1sko <fsaez.perez11@gmail.com>
2024-03-30 11:22:48 +0100

    about

---
+++ front/src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {dev} from '$app/environment';
+    let mode = 'production';
+    if (dev) {
+        mode = 'development';
+    }
+</script>
+This is the contact manager app in {mode} mode.
