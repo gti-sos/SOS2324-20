e5b5718eec28ce4b37c5cd2ac26b98739103541d
Alexfer02 <alexfer202@gmail.com>
2024-04-05 12:22:00 +0200

    error msg

--- front/src/routes/life-expectancy/[Country]/[Year]/search/+page.svelte
+++ front/src/routes/life-expectancy/[Country]/[Year]/search/+page.svelte
@@ -1,36 +1,35 @@
 <script>
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 	import { dev } from '$app/environment';
 	import { page } from '$app/stores';
 
-    let API = '/api/v2/life-expectancy';
+	let API = '/api/v2/life-expectancy';
 	if (dev) {
 		API = 'http://localhost:10000/api/v2/life-expectancy';
 	}
 
-    onMount(() => {
+	onMount(() => {
 		getSearch();
-    });
+	});
 
 	let errorMsg = '';
 	let country = $page.params.Country;
 	let year = $page.params.Year;
-	let data = "";
-	async function getSearch(){
-		try{
+	let data = '';
+	async function getSearch() {
+		try {
 			let response = await fetch(`${API}?country=${country}&year=${year}`);
-			if(response.status === 200){
+			if (response.status === 200) {
 				let datas = await response.json();
 				data = datas;
-				console.log(data);
+				if (datas.length === 0) {
+					errorMsg = 'No se encontraron resultados';
+				}
 			}
-
-		}catch(error){
+		} catch (error) {
 			errorMsg = 'Error loading data';
 		}
-		
 	}
-
 
 	async function deleteLifeExpectancy(country, year) {
 		console.log(`Deleting ${country} for year ${year}`);
@@ -48,7 +47,6 @@
 				console.log('País borrado exitosamente');
 				errorMsg = 'País borrado exitosamente';
 				getSearch();
-				
 			} else {
 				console.log(`Error eliminando el pais, no existe, status code: ${response.status}`);
 			}
@@ -62,22 +60,31 @@
 			deleteLifeExpectancy(country, year);
 		}
 	}
-
 </script>
-
 
 <div class="container">
 	<div class="column">
 		<h1>Esperanza de vida</h1>
 		<p>Resultados para {country} - {year}</p>
+		{#if errorMsg != ''}
+			<hr />
+			<p class="error-msg">{errorMsg}</p>
+		{/if}
 		<ul class="ul-container">
-				<ul class="list-item">
-					<a href="/life-expectancy/{data.country}/{data.year}">{data.country} - {data.year} &nbsp;</a>
-					<div class="buttons">
-						<button class="button" onclick="windows.location.href = '/life-expectancy/{data.country}/{data.year}/edit'">Editar</button>
-						<button class="button" on:click={() => confirmedelete(data.country,data.year)}>Borrar</button>
-					</div>
-				</ul>
+			<ul class="list-item">
+				<a href="/life-expectancy/{data.country}/{data.year}">{data.country} - {data.year} &nbsp;</a
+				>
+				<div class="buttons">
+					<button
+						class="button"
+						onclick="windows.location.href = '/life-expectancy/{data.country}/{data.year}/edit'"
+						>Editar</button
+					>
+					<button class="button" on:click={() => confirmedelete(data.country, data.year)}
+						>Borrar</button
+					>
+				</div>
+			</ul>
 		</ul>
 	</div>
 </div>
--- front/src/routes/life-expectancy/[Country]/search/+page.svelte
+++ front/src/routes/life-expectancy/[Country]/search/+page.svelte
@@ -1,35 +1,34 @@
 <script>
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 	import { dev } from '$app/environment';
 	import { page } from '$app/stores';
 
-    let API = '/api/v2/life-expectancy';
+	let API = '/api/v2/life-expectancy';
 	if (dev) {
 		API = 'http://localhost:10000/api/v2/life-expectancy';
 	}
 
-    onMount(() => {
+	onMount(() => {
 		getSearch();
-    });
+	});
 
 	let errorMsg = '';
 	let country = $page.params.Country;
 	let list_search = [];
-	async function getSearch(){
-		try{
+	async function getSearch() {
+		try {
 			let response = await fetch(`${API}?country=${country}`);
-			if(response.status === 200){
+			if (response.status === 200) {
 				let data = await response.json();
 				list_search = data;
-				console.log(data);
+				if (data.length === 0) {
+					errorMsg = 'No se encontraron resultados';
+				}
 			}
-
-		}catch(error){
+		} catch (error) {
 			errorMsg = 'Error loading data';
 		}
-		
 	}
-
 
 	async function deleteLifeExpectancy(country, year) {
 		console.log(`Deleting ${country} for year ${year}`);
@@ -47,7 +46,6 @@
 				console.log('País borrado exitosamente');
 				errorMsg = 'País borrado exitosamente';
 				getSearch();
-				
 			} else {
 				console.log(`Error eliminando el pais, no existe, status code: ${response.status}`);
 			}
@@ -61,21 +59,29 @@
 			deleteLifeExpectancy(country, year);
 		}
 	}
-
 </script>
-
 
 <div class="container">
 	<div class="column">
 		<h1>Esperanza de vida</h1>
 		<p>Resultados para {country}</p>
+		{#if errorMsg != ''}
+			<hr />
+			<p class="error-msg">{errorMsg}</p>
+		{/if}
 		<ul class="ul-container">
 			{#each list_search as data}
 				<ul class="list-item">
 					<a href="/life-expectancy/{country}/{data.year}">{data.country} - {data.year} &nbsp;</a>
 					<div class="buttons">
-						<button class="button" onclick="windows.location.href = '/life-expectancy/{data.country}/{data.year}/edit'">Editar</button>
-						<button class="button" on:click={() => confirmedelete(data.country,data.year)}>Borrar</button>
+						<button
+							class="button"
+							onclick="windows.location.href = '/life-expectancy/{data.country}/{data.year}/edit'"
+							>Editar</button
+						>
+						<button class="button" on:click={() => confirmedelete(data.country, data.year)}
+							>Borrar</button
+						>
 					</div>
 				</ul>
 			{/each}
