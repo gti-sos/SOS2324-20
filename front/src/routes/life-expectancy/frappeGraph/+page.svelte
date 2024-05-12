05c68303258e82fabbb59707d0e377e299294f8d
Alexfer02 <alexfer202@gmail.com>
2024-05-12 13:18:03 +0200

    frappechart

--- front/src/routes/life-expectancy/frappeGraph/+page.svelte
+++ front/src/routes/life-expectancy/frappeGraph/+page.svelte
@@ -1,20 +1,37 @@
 <script>
 	import { onMount } from 'svelte';
-	import FrappeChart from 'svelte-frappe-charts';
-    import { dev } from '$app/environment';
+	import Chart from 'svelte-frappe-charts';
+	import { dev } from '$app/environment';
 
-	//Muestra si está en desarrollo
 	let API = '/api/v2/life-expectancy';
 	if (dev) {
 		API = 'http://localhost:10000/api/v2/life-expectancy';
 	}
+	let data;
+	let chartRef;
+	async function fillChart() {
+		const response = await fetch(API+'?limit=1000000000&offset=0');
+		const responseData = await response.json();
 
+		let labels = responseData.map(item => [item.country, item.year]);
+		var values = responseData.map(item => item.life_expectancy);
+		let datasets = [ 
+			{ 
+				values: values,
+			}
+		]
 
-    async function obtenerDatos(){
-
-        const response = await fetch(API+'?limit=100000000&offset=0');
-        const data = await response.json();
-
-        let labels = data.array.map(element => element.continent);
-    }
+		data = {
+			labels: labels,
+			datasets: datasets
+		};
+		
+	}
+	onMount(async () => {
+		await fillChart();
+	});
 </script>
+
+<h1>Gráfica de barras de FrappeChart de la esperanza de vida de todos los datos de la bd</h1>
+<Chart {data} type="bar"/>
+
